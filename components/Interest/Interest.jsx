fa3430bd0fea50ce4dd130d41cbd2135e9916ec1
ryan leslie <ryanleslie33@aol.com>
2019-02-15 15:31:58 -0800

    add child component linksk to interest.jsx

--- components/Interest/Interest.jsx
+++ components/Interest/Interest.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 
 
 function Interest(){
+  let interest = {
+    fontSize:"40px",
+    backgroundColor:"grey"
+  }
 
   return (
-    <div  >
-
+    <div style={interest}  >
+    <Link to="/Api">Api Web Services</Link>
+    <br/>
+    <br/>
+    <Link to="/Csharp">C#</Link>
+    <br/>
+    <br/>
+    <Link to="/Teaching">Teaching</Link>
+    <br/>
+    <br/>
     </div>
   );
 }
