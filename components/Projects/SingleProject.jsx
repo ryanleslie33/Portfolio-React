8c001232ae136ab867a00cbe6ec713576a4e030b
ryan leslie <ryanleslie33@aol.com>
2019-02-22 09:22:44 -0800

    add new props component

---
+++ components/Projects/SingleProject.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function SingleProject(props) {
+  let aboutStyle = {
+    fontSize: "50px"
+  }
+  return (
+
+    <div style={aboutStyle}>
+      <h3> name: {props.name}</h3>
+      <h3> technologies: {props.pet}</h3>
+        <h3> description: {props.description}</h3>
+          <h3> status: {props.status}</h3>
+    </div>
+  );
+}
+SingleProject.propTypes = {
+
+  name: PropTypes.string,
+  technologies: PropTypes.string,
+  descriptions: PropTypes.string,
+  status: PropTypes.string,
+};
+
+export default SingleProject;
